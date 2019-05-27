7b158708b60f8f9e15b747d6c575b4312d8f0482
gongjian <gongjian@chengdu-zhifei.com>
2019-05-27 15:31:11 +0800

    添加新的框架

---
+++ CFLMedCab/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CFLMedCab.View;
+using MahApps.Metro.Controls;
+
+namespace CFLMedCab
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private GerFetchView gerFetchView = new GerFetchView();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterGerFetch(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(gerFetchView);
+        }
+
+    }
+}
