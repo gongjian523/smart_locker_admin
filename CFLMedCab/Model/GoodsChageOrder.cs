836ecbcce2fb286056851dbabe29573aa2ac9916
gongjian <gongjian@chengdu-zhifei.com>
2019-06-12 21:33:42 +0800

    添加sqlsugar model 属性

--- CFLMedCab/Model/GoodsChageOrder.cs
+++ CFLMedCab/Model/GoodsChageOrder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,26 +15,31 @@ namespace CFLMedCab.Model
         /// <summary>
         /// 库存变化单号
         /// </summary>
+        [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
         public int id { get; set; }
 
         /// <summary>
         /// 操作人
         /// </summary>
+        [SugarColumn(IsNullable = true)]
         public int operator_id { get; set; }
-        
+
         /// <summary>
         /// 生成时间
         /// </summary>
+        [SugarColumn(IsNullable = true)]
         public DateTime create_time { get; set; }
 
         /// <summary>
         /// 业务类型
         /// </summary>
+        [SugarColumn(IsNullable = true)]
         public int type { get; set; }
 
         /// <summary>
         /// 单据状态 状态：0  待处理；1  已处理。
         /// </summary>
+        [SugarColumn(IsNullable = true)]
         public int status { get; set; }
     }
 }
