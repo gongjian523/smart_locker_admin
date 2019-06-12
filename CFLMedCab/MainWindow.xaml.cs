08b70e118af141f70eca3424ccbf4d5019ab0a94
1329578492@qq.com <1329578492@qq.com>
2019-06-12 08:50:05 +0800

    全屏

--- CFLMedCab/MainWindow.xaml.cs
+++ CFLMedCab/MainWindow.xaml.cs
@@ -337,6 +337,16 @@ namespace CFLMedCab
             }
         }
 
-
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Normal;
+            this.WindowStyle = System.Windows.WindowStyle.None;
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;
+            this.Topmost = true;
+            this.Left = 0.0;
+            this.Top = 0.0;
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+        }
     }
 }
