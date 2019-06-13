b25b12b13be4cb7ae671eb9049f41b8ccd2d7ab5
lizhiwen <lizhiwen@chengdu-zhifei.com>
2019-06-13 17:38:36 +0800

    【修改】增加语音播报，和相关逻辑

--- CFLMedCab/MainWindow.xaml.cs
+++ CFLMedCab/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ using CFLMedCab.DAL;
 using SqlSugar;
 using CFLMedCab.BLL;
 using CFLMedCab.Infrastructure;
+using System.Speech.Synthesis;
 
 namespace CFLMedCab
 {
@@ -92,7 +93,7 @@ namespace CFLMedCab
             //});
             //user.GetList();
 
-            ConsoleManager.Hide();
+            ConsoleManager.Show();
         }
 
 
@@ -426,5 +427,19 @@ namespace CFLMedCab
             Console.WriteLine(strRcv);
 
         }
-    }
+
+		private void TestLocker(object sender, ElapsedEventArgs e)
+		{
+			Console.ReadKey();
+			LockHelper.DelegateGetMsg delegateGetMsg = LockHelper.GetLockerData("COM2", out bool isGetSuccess);
+			delegateGetMsg.DelegateGetMsgEvent += new LockHelper.DelegateGetMsg.DelegateGetMsgHandler(TestLockerEvent);
+		}
+
+		private void TestLockerEvent(object sender, bool isClose)
+		{
+			Console.WriteLine("返回开锁状态{0}", isClose);
+			System.Diagnostics.Debug.WriteLine("返回开锁状态{0}", isClose); 
+		}
+
+	}
 }
