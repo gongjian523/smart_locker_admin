1e5a395fd60baf566ca954c0c6176da098b9c5cd
1329578492@qq.com <1329578492@qq.com>
2019-06-15 16:56:08 +0800

    手术领用关柜显示

---
+++ CFLMedCab/DTO/Stock/GoodsChageOrderdtlDto.cs
@@ -0,0 +1,13 @@
+﻿using CFLMedCab.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFLMedCab.DTO.Stock
+{
+    public class GoodsChageOrderdtlDto: GoodsChageOrderdtl
+    {
+    }
+}
