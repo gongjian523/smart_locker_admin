6d35ced45a7737750c45cc206ca626eeb5e0fcd9
1329578492@qq.com <1329578492@qq.com>
2019-06-15 15:26:12 +0800

    手术领用开柜，耗材详情页面数据绑定

--- CFLMedCab/View/Fetch/SurgeryNumOpen.xaml.cs
+++ CFLMedCab/View/Fetch/SurgeryNumOpen.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using CFLMedCab.BLL;
+using CFLMedCab.DTO.Fetch;
 using CFLMedCab.Model;
+using System;
+using System.Collections.Generic;
 using System.Windows.Controls;
 
 
@@ -18,10 +21,27 @@ namespace CFLMedCab.View.Fetch
         {
             InitializeComponent();
             fetchOrder = model;
-            //SurgeryOrder surgeryOrder = surgeryOrderBll.GetById(fetchOrder.business_order_id);
-            lNum.Content = model.business_order_id;
-            //time.Content = surgeryOrder.surgery_dateiime;
-            //listView.DataContext = fetchOrderdtlBll.GetDetailsUsage(id);
+            SurgeryOrderDto surgeryOrderDto = new SurgeryOrderDto { id = 2, surgery_dateiime = DateTime.Now };
+            surgeryNum.Content = surgeryOrderDto.id;
+            time.Content = surgeryOrderDto.surgery_dateiime;
+            List<SurgeryFetchDto> surgeryFetches = new List<SurgeryFetchDto>();
+            for (int i = 5; i >= 0; i--)
+            {
+                SurgeryFetchDetailsDto surgeryFetch = new SurgeryFetchDetailsDto
+                {
+                    fetch_order_id = i,
+                    goods_name = "注射器",
+                    goods_code = "gr1294",
+                    fetch_type = 1,
+                    wait_num = 2,
+                    fetch_num = 1,
+                    stock_num = 3,
+                    remarks = "麻醉专用"
+                };
+                surgeryFetch.total_num = surgeryFetch.wait_num + surgeryFetch.fetch_num;
+                surgeryFetches.Add(surgeryFetch);
+            }
+            listView.DataContext = surgeryFetches;
         }
         
     }
