0af858524394579009f47e784e2558a7911c0c4f
gongjian <gongjian@chengdu-zhifei.com>
2019-06-16 16:05:48 +0800

    修改测试

--- CFLMedCab/Test/TestGoods.cs
+++ CFLMedCab/Test/TestGoods.cs
@@ -1,6 +1,8 @@
 ﻿using CFLMedCab.DAL;
+using CFLMedCab.Infrastructure;
 using CFLMedCab.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,10 +21,16 @@ namespace CFLMedCab.Test
 
         public void AddGoodTest()
         {
+
+            Hashtable ht = new Hashtable();
+            HashSet<string> hs1 = new HashSet<string> { "E20000176012027919504D98", "E20000176012025319504D67", "E20000176012025619504D70", "E20000176012028119504DA5", "E20000176012023919504D48" };
+            ht.Add("COM1", hs1);
+            HashSet<string> hs4 = new HashSet<string> { "E20000176012028219504DAD", "E20000176012026619504D8D", "E20000176012026319404F98", "E20000176012028019504DA0", "E20000176012026519504D85" };
+            ht.Add("COM4", hs4);
+            ApplicationState.SetValue((int)ApplicationKey.CurGoods, ht);
+
             if (GoodsDal.GetGoodsNum() > 0)
                 return;
-
-
 
             List<Goods> goods = new List<Goods>{
                 new Goods
