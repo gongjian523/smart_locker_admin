e2637754a9616c1223d0804933feadee6329d259
rhj <hongjun_ren@163.com>
2019-06-17 20:29:09 +0800

    增加Label样式

--- CFLMedCab/App.xaml.cs
+++ CFLMedCab/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CFLMedCab.DAL;
 using CFLMedCab.Infrastructure;
+using CFLMedCab.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,6 +26,14 @@ namespace CFLMedCab
             HashSet<string> hs4 = new HashSet<string> { "E20000176012028219504DAD", "E20000176012026619504D8D", "E20000176012026319404F98", "E20000176012028019504DA0", "E20000176012026519504D85" };
             ht.Add("COM4", hs4);
             ApplicationState.SetValue((int)ApplicationKey.CurGoods, ht);
+
+            User user = new User
+            {
+                id = 1111,
+                name = "Nathan",
+                vein_id = 12323,
+            };
+            ApplicationState.SetValue((int)ApplicationKey.CurUser, user);
         }
     }
 }
