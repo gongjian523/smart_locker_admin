e8ce867e827bc768c0aa9bce55b02689e882792b
1329578492@qq.com <1329578492@qq.com>
2019-06-19 16:17:16 +0800

    页面枚举调整

---
+++ CFLMedCab/Converter/OperateTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using CFLMedCab.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CFLMedCab.Converter
+{
+    public class OperateTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((OperateType)value == OperateType.入库)
+                return "入库";
+            else
+                return "出库";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
