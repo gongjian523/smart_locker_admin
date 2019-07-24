a2f6864de05fce90e1dd56fb4e81957815a5ae41
致文 黎 <as504561182@outlook.com>
2019-07-24 17:49:38 +0800

    【修改】增加商品单体的异常显示字段

---
+++ CFLMedCab/Http/Enum/AbnormalDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFLMedCab.Http.Enum
+{
+	/// <summary>
+	/// 异常展示
+	/// </summary>
+	public enum AbnormalDisplay
+	{
+		/// <summary>
+		/// 正常
+		/// </summary>
+		[Description("正常")]
+		正常 = 1,
+
+		/// <summary>
+		/// 异常 
+		/// </summary>
+		[Description("异常")]
+		异常 = 2
+
+	}
+}
