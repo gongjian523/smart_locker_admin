2f2c8583180a3a7d9746f9035de37544b981cda3
致文 黎 <as504561182@outlook.com>
2019-08-06 18:14:43 +0800

    【修改】定时计划切换为正式环境

--- CFLMedCab/Infrastructure/QuartzHelper/job/GetInventoryPlanJoB.cs
+++ CFLMedCab/Infrastructure/QuartzHelper/job/GetInventoryPlanJoB.cs
@@ -19,7 +19,7 @@ namespace CFLMedCab.Infrastructure.QuartzHelper.job
 		{
 			Console.WriteLine("GetInventoryPlanJoB进入");
 
-			var baseDataInventoryPlan = InventoryTaskBll.GetInstance().GetInventoryPlanByEquipmnetNameOrId("AQACQqweDg8BAAAAFUD8WDEPsxV_FwQA");
+			var baseDataInventoryPlan = InventoryTaskBll.GetInstance().GetInventoryPlanByEquipmnetNameOrId(ApplicationState.GetEquipId());
 
 			//校验是否含有数据，如果含有数据，进行后续操作
 			HttpHelper.GetInstance().ResultCheck(baseDataInventoryPlan, out bool isSuccess);
@@ -27,32 +27,13 @@ namespace CFLMedCab.Infrastructure.QuartzHelper.job
 			if (isSuccess)
 			{
 				var inventoryPlans = baseDataInventoryPlan.body.objects;
-				inventoryPlans.ForEach(item =>
-				{
-					item.InventoryTime = "17:57:00";
-				});
-
 				//cronStr = "0 43 11 * * ?";
 				CustomizeScheduler.GetInstance().SchedulerStartOrUpdateOrDeleteByPlans(inventoryPlans).Wait();
-
-
-				inventoryPlans.ForEach(item =>
-				{
-					item.InventoryTime = "17:59:00";
-				});
-
-
-				InventoryPlan inventoryPlan = inventoryPlans[0].MapTo<InventoryPlan>();
-
-				inventoryPlan.id = inventoryPlan.id + "134";
-				inventoryPlan.InventoryTime = "18:01:00";
-
-				inventoryPlans.Add(inventoryPlan);
-
-				//模拟重复任务
-				CustomizeScheduler.GetInstance().SchedulerStartOrUpdateOrDeleteByPlans(inventoryPlans).Wait();
-
 				//cronStr = "0 44 11 * * ?";
+			}
+			else
+			{
+				LogUtils.Error($"拉取盘点定时计划失败！");
 			}
 
 			return null;
