3ceba5e9d29c85938affc5e48e958a57ec267576
gongjian <gongjian@chengdu-zhifei.com>
2020-01-10 21:01:03 +0800

    添加需要的文件

---
+++ CFLMedCab/Http/Model/CommodityCatalogue.cs
@@ -0,0 +1,17 @@
+﻿using CFLMedCab.Http.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFLMedCab.Http.Model
+{
+    /// <summary>
+    /// 商品目录
+    /// </summary>
+    public class CommodityCatalogue : BaseModel
+    {
+
+    }
+}
