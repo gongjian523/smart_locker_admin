007191702f1ea5660e29f9a797db45f0859f2463
gongjian <jian.gong@istuary.com>
2020-08-09 23:23:42 +0800

    添加开柜记录和登录信息的mode

---
+++ CFLMedCab/Model/LoginDetail.cs
@@ -0,0 +1,62 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFLMedCab.Model
+{
+    public class LoginDetail
+    {
+		/// <summary>
+		/// ID
+		/// </summary>
+		[SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+		public int id { get; set; }
+
+		/// <summary>
+		/// 登录ID
+		/// </summary>
+		public string loginId { get; set; }
+
+		/// <summary>
+		/// 登录时间
+		/// </summary>
+		public DateTime logintime { get; set; }
+
+		/// <summary>
+		/// 操作
+		/// </summary>
+		public string operate { get; set; }
+
+
+		/// <summary>
+		/// 目录名字
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string CatalogueName { get; set; }
+
+
+		/// <summary>
+		/// 规格
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string Specifications { get; set; }
+
+
+		/// <summary>
+		/// 货位
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string position { get; set; }
+
+
+		/// <summary>
+		/// 出入库
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string InOrOut { get; set; }
+
+	}
+}
---
+++ CFLMedCab/Model/LoginInfo.cs
@@ -0,0 +1,53 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFLMedCab.Model
+{
+    public class LoginInfo
+    {
+		/// <summary>
+		/// 登录ID
+		/// </summary>
+		[SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+		public int id { get; set; }
+
+		/// <summary>
+		/// 姓名
+		/// </summary>
+		public string name { get; set; }
+
+		/// <summary>
+		/// 用户名,手机号码，带有+86
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string username { get; set; }
+
+		/// <summary>
+		/// 科室
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string department { get; set; }
+
+		/// <summary>
+		/// 登录时间
+		/// </summary>
+		public DateTime logintime { get; set; }
+
+
+		/// <summary>
+		/// 登出时间
+		/// </summary>
+		public DateTime logouttime { get; set; }
+
+		/// <summary>
+		/// 退出类型
+		/// </summary>
+		[SugarColumn(IsNullable = true)]
+		public string logoutInfo  { get; set; }
+
+	}
+}
